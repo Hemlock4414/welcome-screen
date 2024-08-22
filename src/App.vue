2785f2279f65d1132cdc3761a2cbc34ef2a8d0f6
Hemlock4414 <antonio.marrara@gmx.ch>
2024-08-22 16:55:19 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,7 @@
         this.currentDate = this.getCurrentDate();
         setInterval(() => {
           this.currentDate = this.getCurrentDate();
-        }, 3600000); // Aktualisiere jede Stunde
+        }, 3600000);          // Aktualisiere jede Stunde
       }
     },
     mounted() {
@@ -35,43 +35,43 @@
         <div class="topline">Welcome to Opportunity</div>
 
         <div class="date"> {{ currentDate }} </div>
-    </header>
+  </header>
 
-    <main>
-        <div class="monitor">
-            <div class="inner-monitor">
-              <ul>
-                <li>
-                  <div class="text-top">
-                    <p>14.00 Uhr</p>
-                  </div>
-                </li>
-                <li>
-                  <div class="text-middle">
-                    <p>Basisbeschäftigung Besuch</p>
-                  </div>
-                </li>
-                <li>
-                  <div class="text-bottom">
-                    <p>Interessierte für den zweiten Kurs werden uns besuchen</p>
-                  </div>
-                </li> 
-              </ul>
-            </div>
+  <main>
+    <div class="monitor">
+        <div class="inner-monitor">
+          <ul>
+            <li>
+              <div class="text-top">
+                <p>14.00 Uhr</p>
+              </div>
+            </li>
+            <li>
+              <div class="text-middle">
+                <p>Basisbeschäftigung Besuch</p>
+              </div>
+            </li>
+            <li>
+              <div class="text-bottom">
+                <p>Interessierte für den zweiten Kurs werden uns besuchen</p>
+              </div>
+            </li> 
+          </ul>
         </div>
+    </div>
 
-        <div class="monitor"></div>
-        <div class="monitor"></div>
+    <div class="monitor"></div>
+    <div class="monitor"></div>
 
-    </main>
+  </main>
 
-    <footer>
-      <div class="footer-container">
-        <img src="/STZH_SEB_Logo-2.png" alt="">
-        <img src="/Opportunity.png" alt="">
-        <img src="/SAG_Logo_De.png" alt="">
-      </div>
-    </footer>
+  <footer>
+    <div class="footer-container">
+      <img src="/STZH_SEB_Logo-2.png" alt="">
+      <img src="/Opportunity.png" alt="">
+      <img src="/SAG_Logo_De.png" alt="">
+    </div>
+  </footer>
 
 </template>
 
