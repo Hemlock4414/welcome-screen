be685f297a12cf49a6caf07a5c9894235a5769ca
Hemlock4414 <antonio.marrara@gmx.ch>
2024-09-09 15:57:17 +0200

    css-update

--- src/components/MonitorCard.vue
+++ src/components/MonitorCard.vue
@@ -31,7 +31,58 @@ export default {
 </script>
 
 <style scoped>
+
+
 .monitor {
+  margin: 40px 60px; 
+  background-color: #0F05A0;
+  min-height: 182px;
+}
+
+.inner-monitor {
+  padding: 34.5px 35px 39.5px 35px; /* Zusammengefasst: oben/rechts/unten/links */
+  min-height: 100px;
+}
+
+.text-top {
+  color: #EB5E00;
+  font-weight: 900;
+  font-size: 28px;
+  line-height: 36px;
+}
+
+.text-middle, .text-bottom { 
+  color: #FFBFAB;
+  font-size: 28px;
+  line-height: 36px;
+}
+
+.text-middle {
+  font-weight: 900;
+  word-break: break-word;
+}
+
+.text-bottom {
+  font-weight: 500;
+}
+
+@media (max-width: 481px) {
+  header {
+    font-size: 14vw;
+    margin-left: auto;
+    margin-right: auto;
+    text-align: center;
+  }
+
+  .topline, .date {
+    margin-left: auto;
+    margin-right: auto;
+  }
+}
+
+
+
+/* .monitor {
   margin-left: 60px;
   margin-right: 60px;
   margin-top: 40px;
@@ -81,5 +132,5 @@ export default {
     margin-left: auto;
     margin-right: auto;
   }
-}
+} */
 </style>
